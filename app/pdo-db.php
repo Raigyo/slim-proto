73ced91371a0a5d40d0eb08b53153fe61d0bb50b
Raigyo <vincent.chilot@gmail.com>
2019-02-20 16:54:02 +0100

    db connexion not working

--- app/pdo-db.php
+++ app/pdo-db.php
@@ -1,13 +1,22 @@
 <?php
 
 /*
+sudo apt-get install php-pgsql
 sudo atom /etc/php/7.3/cli/php.ini
 Uncomment:
 extension=pgsql
 extension=pdo_pgsql
++
 Use https://github.com/becodeorg/LIE-Jepsen-1.9/tree/master/02-the-hill/docker-compose-postgres
 docker-compose up
 */
 
+try
+   {
+       $pdo = new PDO('pgsql:host=localhost:9000;port=5432;dbname=becode', 'becode', 'becode');
+   }
+   catch (PDOException $e)
+   {
+       echo $e->getMessage();
 
-$conn = new PDO('pgsql:host=localhost:9000;port=5432;dbname=becode', 'becode', 'becode');
+   }
